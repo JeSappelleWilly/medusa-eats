d7cecd2ce9a4858eb67a9488df7f2ca8092166d9
JeSappelleWilly <wserge98@gmail.com>
2025-05-01 08:56:17 +0100

    Update vite.config.js

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   server: {
-    allowedHosts: ['backend-production-a427.up.railway.app'],
+    allowedHosts: true,
     cors: true // This enables CORS with default options
   }
 });
